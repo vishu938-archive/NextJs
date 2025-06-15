1aa82e4df3274f8bc77dba9547e23e09e38c2767
vishu938-archive <vishalvinayak0000@gmail.com>
2025-06-15 19:24:19 +0530

    NextJs Catch All Segments

---
+++ src/app/docs/[[...slug]]/page.jsx
@@ -0,0 +1,24 @@
+// The folder name '[[...slug]]' uses Next.js dynamic catch-all routing.
+// This means any route like /docs/a, /docs/a/b, etc., will be handled by this file.
+
+import React from "react";
+
+const Document = ({ params }) => {
+  // 'params' is provided by Next.js for dynamic routes.
+  // 'slug' is an array containing the path segments after /docs/.
+  const { slug } = params;
+
+  if (slug?.length === 2) {
+    return (
+      <h3>
+        Viewing docs for feature {slug[0]} and concept {slug[1]}
+      </h3>
+    );
+  } else if (slug?.length === 1) {
+    return <h3>Viewing docs for feature {slug[0]}</h3>;
+  }
+
+  return <h3>Viewing docs for all features</h3>;
+};
+
+export default Document;
