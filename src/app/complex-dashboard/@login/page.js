ee649a0258faeac89a9b667425749bd75fccd651
vishu938-archive <vishalvinayak0000@gmail.com>
2025-06-20 01:41:30 +0530

    Conditional Routes in concept of parallel routes

---
+++ src/app/complex-dashboard/@login/page.js
@@ -0,0 +1,8 @@
+import { Card } from "@/components/card";
+import React from "react";
+
+const LoginSlot = () => {
+  return <Card>Please login to Continue</Card>;
+};
+
+export default LoginSlot;
--- src/app/complex-dashboard/layout.js
+++ src/app/complex-dashboard/layout.js
@@ -7,9 +7,18 @@ import React from "react";
 // These are called parallel routes. They allow you to render multiple pages or components side-by-side within the same layout.
 // Each parallel route is rendered in a separate slot, so you can show different sections (like notifications, revenue, users) at the same time on the dashboard.
 // This is useful for building complex UIs where multiple independent views need to be displayed together.
+// the layout.js receives the children prop along with the parallel routes as props.
+// The children prop contains the main content of the page like the dashboard while the parallel routes provide additional sections like notifications, revenue, and users.
 
-const DashboardLayout = ({ children, users, notifications, revenue }) => {
-  return (
+const DashboardLayout = ({
+  children,
+  users,
+  notifications,
+  revenue,
+  login,
+}) => {
+  const isUserLoggedIn = true   ; // This should be replaced with actual authentication logic
+  return isUserLoggedIn ? (
     <div>
       <div>{children}</div>
       <div style={{ display: "flex" }}>
@@ -20,6 +29,8 @@ const DashboardLayout = ({ children, users, notifications, revenue }) => {
         <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
       </div>
     </div>
+  ) : (
+    login
   );
 };
 
